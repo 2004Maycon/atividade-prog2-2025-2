ce7916e8d5ec5db32970a4ebc6ce599e92ead597
2004Maycon <pequenosilvamaycon@gmail.com>
2025-08-13 15:09:35 -0300

    Update atividade1.12.1.py

--- teste.txt/atividade1.12.1.py
+++ teste.txt/atividade1.12.1.py
@@ -1,10 +1,11 @@
 def main():
-  idade= int(input ("qual a sua idade"))
+  idade= int(input ("qual a sua idade: "))
   quantidade=0
+  idade_total = 0
   while idade != 0:
-    idade_total +=idade
+    idade_total+= idade
     quantidade+=1
-    idade= int(input ("qual a sua idade"))
+    idade= int(input ("qual a sua idade: "))
   idade_media= idade_total/quantidade
-  print(f"a idade média é {idade_media}")
+  print(f"a idade média é {idade_media :.2f}")
 main()
