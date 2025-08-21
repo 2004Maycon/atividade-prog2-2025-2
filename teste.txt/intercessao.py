995dd016947ab9dfffef7780fa652f99e5a51fe0
2004Maycon <pequenosilvamaycon@gmail.com>
2025-08-21 15:59:37 -0300

    Update intercessao.py

--- teste.txt/intercessao.py
+++ teste.txt/intercessao.py
@@ -7,8 +7,8 @@ def F_uniao(a,b):
 				intercessao.append(a[i])
 	return intercessao
 def main():
-	lista1 = (1,2,3,4,6,8)
-	lista2= (1,4,5,8,7,6,4,9,10)
+	lista1 = (1,2,3,4,6,8,9,11)
+	lista2= (1,4,5,8,2,7,6,9,10)
 	uniao = F_uniao(lista1,lista2)
 	print(lista1)
 	print(lista2)
