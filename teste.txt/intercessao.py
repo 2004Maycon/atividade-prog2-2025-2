56fe3721314027d32beb18c08ccb26187d4a3ec4
2004Maycon <pequenosilvamaycon@gmail.com>
2025-08-21 15:39:28 -0300

    Create intercessao.py

---
+++ teste.txt/intercessao.py
@@ -0,0 +1,18 @@
+def F_uniao(a,b):
+	intercessao = []
+
+	for i in range (len(a)):
+		for j in range (len(b)):
+			if a[i] == b[j]:
+				intercessao.append(a[i])
+	return intercessao
+def main():
+	lista1 = (1,2,3,4,6,8)
+	lista2= (1,4,5,8,7,6,4,9,10)
+	uniao = F_uniao(lista1,lista2)
+	print(lista1)
+	print(lista2)
+	print(uniao)
+	print()
+	return 0
+main()
